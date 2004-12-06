55cfd1d73d7e5eb6bdee30fdd7f62bf4be46ebd2
vances <vances@378e65c5-da2b-0410-8185-01596a0a59de>
2004-12-06 19:34:03 +0000

    Removed debug option. 	Changed variable name to suppress warning.

--- examples/abis_emulator/abis_emulator_tei_sup.erl
+++ examples/abis_emulator/abis_emulator_tei_sup.erl
@@ -3,9 +3,9 @@
 -export([init/1]).
 
 init([Sup, TEI]) ->
-	BSCStartFunc = {gen_fsm, start_link, [abis_emulator_bsc_fsm, [Sup, TEI], [{debug, [trace]}]]},
+	BSCStartFunc = {gen_fsm, start_link, [abis_emulator_bsc_fsm, [Sup, TEI], []]},
 	BSCChildSpec = {bsc, BSCStartFunc, permanent, 4000, worker, [abis_emulator_bsc_fsm]},
-	BTSStartFunc = {gen_fsm, start_link, [abis_emulator_bts_fsm, [Sup, TEI], [{debug, [trace]}]]},
+	BTSStartFunc = {gen_fsm, start_link, [abis_emulator_bts_fsm, [Sup, TEI], []]},
 	BTSChildSpec = {bts, BTSStartFunc, permanent, 4000, worker, [abis_emulator_bts_fsm]},
 	{ok, {{one_for_all, 0, 1}, [BSCChildSpec, BTSChildSpec]}}.
 
