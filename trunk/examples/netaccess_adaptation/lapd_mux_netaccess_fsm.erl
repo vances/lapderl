391994695d6627a4e07d1fe9d2c48853c420dae7
vances <vances@378e65c5-da2b-0410-8185-01596a0a59de>
2005-01-02 00:50:37 +0000

    Changed to a BSD license.

--- trunk/examples/netaccess_adaptation/lapd_mux_netaccess_fsm.erl
+++ trunk/examples/netaccess_adaptation/lapd_mux_netaccess_fsm.erl
@@ -1,12 +1,37 @@
 %%%---------------------------------------------------------------------
-%%% @copyright Motivity Telecom Inc. 2004
+%%% @copyright 2004,2005 Motivity Telecom Inc.
 %%% @end
 %%%
-%%% All rights reserved. No part of this computer program(s) may be
-%%% used, reproduced, stored in any retrieval system, or transmitted,
-%%% in any form or by any means, electronic, mechanical, photocopying,
-%%% recording, or otherwise without prior written permission of
-%%% Motivity Telecom Inc.
+%%% Copyright (c) 2004,2005 Motivity Telecom Inc.
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions
+%%% are met:
+%%%       - Redistributions of source code must retain the above
+%%%         copyright notice, this list of conditions and the following
+%%%         disclaimer.
+%%%       - Redistributions in binary form must reproduce the above
+%%%         copyright notice, this list of conditions and the following
+%%%         disclaimer in the documentation and/or other materials 
+%%%         provided with the distribution.
+%%%       - Neither the name of Motivity Telecom Inc. nor the names of
+%%%         its contributors may be used to endorse or promote products
+%%%         derived from this software without specific prior written
+%%%         permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+%%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+%%% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+%%% COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+%%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+%%% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+%%% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+%%% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+%%% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+%%% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+%%% POSSIBILITY OF SUCH DAMAGE.
 %%%---------------------------------------------------------------------
 %%%
 %%% @author Vance Shipley <vances@motivity.ca>
@@ -15,9 +40,16 @@
 %%% 	<p>This module adapts the netaccess implementation of layer 1 to
 %%% 	the LAPD layer 2 implementation.</p>
 %%%
+%%% @reference <a href="index.html">The LAPD User's Guide</a>
+%%% @reference <a href="http://www.motivity.ca/netaccess">The netaccess User's Guide</a>
+%%%
+%%% @private
+         
+-author('vances@motivity.ca').
+
          
 -module(lapd_mux_netaccess_fsm).
--copyright('Copyright (c) 2004 Motivity Telecom Inc.').
+-copyright('Copyright (c) 2004,2005 Motivity Telecom Inc.').
 -author('vances@motivity.ca').
 -behaviour(lapd_mux_fsm).
 
