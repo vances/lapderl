c1f3192d44ac42c482d23a6caeb354854e496187
vances <vances@378e65c5-da2b-0410-8185-01596a0a59de>
2005-01-02 00:50:37 +0000

    Changed to a BSD license.

--- examples/abis_emulator/abis_emulator_app.erl
+++ examples/abis_emulator/abis_emulator_app.erl
@@ -1,4 +1,55 @@
+%%%---------------------------------------------------------------------
+%%% @copyright 2004,2005 Motivity Telecom Inc.
+%%% @end
+%%%
+%%% Copyright (c) 2004,2005 Motivity Telecom Inc.
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions
+%%% are met:
+%%%       - Redistributions of source code must retain the above
+%%%         copyright notice, this list of conditions and the following
+%%%         disclaimer.
+%%%       - Redistributions in binary form must reproduce the above
+%%%         copyright notice, this list of conditions and the following
+%%%         disclaimer in the documentation and/or other materials 
+%%%         provided with the distribution.
+%%%       - Neither the name of Motivity Telecom Inc. nor the names of
+%%%         its contributors may be used to endorse or promote products
+%%%         derived from this software without specific prior written
+%%%         permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+%%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+%%% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+%%% COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+%%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+%%% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+%%% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+%%% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+%%% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+%%% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+%%% POSSIBILITY OF SUCH DAMAGE.
+%%%---------------------------------------------------------------------
+%%%
+%%% @author Vance Shipley <vances@motivity.ca>
+%%%
+%%% @doc Application callback module for the abis_emulator application.
+%%%	<p>The abis_emulator application provides a simple way to 
+%%% 	recreate a GSM Abis protocol session.  The purpose of this
+%%% 	application is to create an test environment for probe 
+%%% 	development.</p>
+%%% @end
+%%%
+%%% @reference <a href="index.html">The LAPD User's Guide</a>
+%%%
+%%% @private
+         
 -module(abis_emulator_app).
+-copyright('Copyright (c) 2004,2005 Motivity Telecom Inc.').
+-author('vances@motivity.ca').
 -behaviour(application).
 -export([start/2, stop/1]).
 
@@ -13,3 +64,4 @@ start(normal, _Args) ->
 	supervisor:start_link(abis_emulator_sup, StartArgs).
 	
 stop(_State) -> ok.
+
