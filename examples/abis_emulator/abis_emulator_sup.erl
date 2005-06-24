8d8fe4226f64bab715b6243f2b11d283abda23ce
vances <vances@378e65c5-da2b-0410-8185-01596a0a59de>
2005-06-24 17:31:11 +0000

    Changes to support multiple links.

--- examples/abis_emulator/abis_emulator_sup.erl
+++ examples/abis_emulator/abis_emulator_sup.erl
@@ -48,27 +48,26 @@
 -behaviour(supervisor).
 -export([init/1]).
 
-init([NAServerName, BoardName, BoardNumber, BSCLapdId, BTSLapdId, TEIs]) ->
-	NAChildSpec = init_na(NAServerName, BoardName, BoardNumber),
-	BSCChildSpec = init_lapd(bsc, NAServerName, BSCLapdId),
-	BTSChildSpec = init_lapd(bts, NAServerName, BTSLapdId),
-	TEIChildSpecs = init_tei(TEIs, []),
-	{ok, {{one_for_one, 0, 1}, [NAChildSpec, BSCChildSpec, BTSChildSpec] ++ TEIChildSpecs}}.
+init(Args) ->
+	init(Args, 0, []).
 
-init_na(NAServerName, BoardName, BoardNumber) ->
-	StartArgs = [NAServerName, BoardName, BoardNumber],
-	StartFunc = {netaccess, start_link, StartArgs},
-	{na, StartFunc, permanent, 4000, worker, [netaccess]}.
+init([{netaccess, Args}|T], LinkCount, Acc) ->
+	ChildSpec = init_na(Args),
+	init(T, LinkCount, [ChildSpec|Acc]);
+init([{abis, Args}|T], LinkCount, Acc) ->
+	ChildSpec = init_link(LinkCount, Args),
+	init(T, LinkCount + 1,  [ChildSpec|Acc]);
+init([], _, Acc) ->
+	{ok, {{one_for_one, 0, 1}, list:reverse(Acc)}}.
 
-init_lapd(Mode, NA, LapdId) ->
-	StartArgs = [lapd_mux_netaccess_fsm, [NA, LapdId], []],
-	StartFunc = {lapd, start_link, StartArgs},
-	{Mode, StartFunc, permanent, 4000, worker, [lapd, lapd_mux_netaccess_fsm]}.
+init_na(Args) ->
+	StartArgs = [abis_emulator_na_sup, Args],
+	StartFunc = {supervisor, start_link, StartArgs},
+	{na_sup, StartFunc, permanent, infinity, supervisor, []}.
 
-init_tei([TEI|T], ChildSpecs) ->
-	StartFunc = {supervisor, start_link, [abis_emulator_tei_sup, [self(), TEI]]},
-	ChildSpec = {TEI, StartFunc, permanent, infinity, supervisor, [abis_emulator_tei_sup]},
-	init_tei(T, ChildSpecs ++ [ChildSpec]);
-init_tei([], ChildSpecs) ->
-	ChildSpecs.
+init_link(LinkCount, Args) ->
+	StartArgs = [abis_emulator_link_sup, Args],
+	StartFunc = {supervisor, start_link, StartArgs},
+	Name = list_to_atom("link_sup_" ++ integer_to_list(LinkCount)),
+	{Name, StartFunc, permanent, infinity, supervisor, []}.
 
